602a8d1ca6d7307fd61cc43ed046520dffb1ecc3
SSerranoCesar22 <144110068+SSerranoCesar22@users.noreply.github.com>
2023-12-01 23:02:39 +0100

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,13 @@
+
+let dia;
+function redirigirAReservas() {
+    window.location.href = "reservas.html";
+  }
+function redirigirHome() {
+    window.location.href = "index.html";
+  }
+
+function seleccionarHora(dia){
+    valor = dia;
+    window.location.href = "form.html";
+}
